2d906dcf94e2b9c2aeb0c330a18f48ad1912a71c
Oleksandr Veselovskyi <val.oleksandr.veselovskyi@lampenwelt.de>
2025-02-17 17:27:15 +0200

    Update

---
+++ entry/edition/main/sample.env.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+    'mode' => 'dev',
+    'router' => [
+        'bypass' => [
+            'rules' => [
+                'url_starts_with' => [],
+            ],
+        ],
+    ],
+    'session' => [
+        // 'file' and 'redis' are supported out of the box
+        // 'redis' requires session_redis storage to be configured in storage section
+        'handler' => 'file',
+    ],
+    'storage' => [
+
+        // Additional storages if needed
+        //'redis' => [
+        //    'options' => [
+        //        'host' => 'localhost',
+        //        'port' => 6379,
+        //        'db' => 0,
+        //        'prefix' => '',
+        //    ],
+        //],
+        //'database' => [
+        //    'options' => [
+        //        'host' => 'localhost',
+        //        'port' => '3306',
+        //        'username' => '****',
+        //        'password' => '****',
+        //        'dbname' => '****',
+        //        'shared' => 'SHARED_PDO_CONNECTION',
+        //    ],
+        //],
+    ],
+    'project' => [
+        // Give Lightna access to app database
+        //'connection' => [
+        //    'host' => 'localhost',
+        //    'port' => '3306',
+        //    'username' => '****',
+        //    'password' => '****',
+        //    'dbname' => '****',
+        //],
+    ],
+    // Configure Elasticsearch if needed
+    //'elasticsearch' => [
+    //    'connection' => [
+    //        'host' => 'localhost',
+    //        'port' => 9200,
+    //    ],
+    //    'prefix' => '****',
+    //],
+];
